64d902e40740cd34ba39b15c58ca0ba5e49df3dd
Adam Stein <adaminsky@gmail.com>
2017-07-23 20:17:35 -0700

    Made the login screen with Firebase and a basic orderpage.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,34 +2,49 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { AngularFireAuthModule } from "angularfire2/auth";
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+import { HomePage } from '../pages/home/home';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AngularFireModule } from 'angularfire2';
+import { HttpModule } from '@angular/http';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { FIREBASE_CONFIG } from './app.firebase.config';
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
-    HomePage,
-    TabsPage
+      TabsPage,
+      LoginPage,
+      RegisterPage,
+      HomePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+      IonicModule.forRoot(MyApp),
+      HttpModule,
+      AngularFireDatabaseModule,
+      AngularFireModule.initializeApp(FIREBASE_CONFIG),
+      AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     AboutPage,
     ContactPage,
-    HomePage,
-    TabsPage
+      TabsPage,
+      LoginPage,
+      RegisterPage,
+      HomePage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,29 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    menu: FirebaseListObservable<any[]>;
+
+    constructor(public afd: AngularFireDatabase, private afAuth: AngularFireAuth, private toast: ToastController, public navCtrl: NavController) {
+        this.menu = this.afd.list('/Menu/milk_tea');
+        console.log(this.menu);
 
   }
 
+    pageWillLoad() {
+        this.afAuth.authState.subscribe(data => {
+            if (data && data.email && data.uid) {
+                this.toast.create({
+                    message: "Welcome to BobaGo, ${data.email}",
+                    duration: 3000
+                }).present();
+            }
+            else {
+                this.toast.create({
+                    message: "Could not find your authentication details.",
+                    duration: 3000
+                }).present();
+            }
+        });
+    }
+    
 }
